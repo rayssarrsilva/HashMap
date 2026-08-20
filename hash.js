c1bf5fb7b29eb4bd6e5bd8e81b608c7e24f1c142
Rayssa <rayssarrsilva1@gmail.com>
2026-08-20 15:50:07 -0300

    Create the hash function generator
    it transform a key into an indice of the bucket

---
+++ hash.js
@@ -0,0 +1,10 @@
+function hash(key, capacity) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+    hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % capacity;
+    }
+
+    return hashCode;
+} 
