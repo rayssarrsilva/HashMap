0142bdc65da33d3bf1083ed13706d228dbeb2385
Rayssa <rayssarrsilva1@gmail.com>
2026-08-20 15:56:50 -0300

    Create the Hashmap class with its constructor
    the createbuckets method will be created

---
+++ hashmap.js
@@ -0,0 +1,8 @@
+class HashMap {
+    constructor(loadFactor = 0.75, capacity = 16){
+        this.loadFactor = loadFactor;
+        this.capacity = capacity;
+        this.buckets = this.#createBuckets(this.capacity);
+        this.size = 0;
+    }
+}
