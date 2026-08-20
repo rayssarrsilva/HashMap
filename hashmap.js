d4599b01e93dde08c76e68d3decee7c648222ae9
Rayssa <rayssarrsilva1@gmail.com>
2026-08-20 16:20:49 -0300

    Implement the desired functions

--- hashmap.js
+++ hashmap.js
@@ -9,4 +9,98 @@ class HashMap {
     #createBuckets(capacity){
         return Array.from({length: capacity}, () => []);
     }
+
+    #hash(key) {
+        return hash(key, this.capacity);
+    }
+
+    #checkIndex(index) {
+        if (index < 0 || index >= this.buckets.length) {
+        throw new Error('Trying to access index out of bounds');
+        }
+    }
+
+    #findEntry(bucket, key) {
+        return bucket.find((entry) => entry[0] === key);
+    }
+    
+    #grow() {
+        const oldEntries = this.entries();
+    
+        this.capacity *= 2;
+        this.buckets = this.#createBuckets(this.capacity);
+        this.size = 0;
+    
+        for (const [key, value] of oldEntries) {
+            this.set(key, value);
+        }
+    }
+
+    entries() {
+        return this.buckets.flat();
+    }
+
+    set(key, value) {
+        const index = this.#hash(key);
+        this.#checkIndex(index);
+        const bucket = this.buckets[index];
+    
+        const existingEntry = this.#findEntry(bucket, key);
+        if (existingEntry) {
+        existingEntry[1] = value; 
+        return;
+        }
+    
+        bucket.push([key, value]);
+        this.size++;
+    
+        if (this.size / this.capacity > this.loadFactor) {
+        this.#grow();
+        }
+    }
+
+    get(key) {
+        const index = this.#hash(key);
+        this.#checkIndex(index);
+        const entry = this.#findEntry(this.buckets[index], key);
+        return entry ? entry[1] : null;
+    }
+    
+    has(key) {
+        const index = this.#hash(key);
+        this.#checkIndex(index);
+        return this.#findEntry(this.buckets[index], key) !== undefined;
+    }
+
+    remove(key) {
+        const index = this.#hash(key);
+        this.#checkIndex(index);
+        const bucket = this.buckets[index];
+        const entryIndex = bucket.findIndex((entry) => entry[0] === key);
+    
+        if (entryIndex === -1) return false;
+    
+        bucket.splice(entryIndex, 1);
+        this.size--;
+        return true;
+    }
+
+    length() {
+        return this.size;
+    }
+
+    clear() {
+        this.buckets = this.#createBuckets(this.capacity);
+        this.size = 0;
+    }
+
+    keys() {
+        return this.entries().map(([key]) => key);
+    }
+    
+    values() {
+        return this.entries().map(([, value]) => value);
+    }
 }
+
+export default HashMap;
